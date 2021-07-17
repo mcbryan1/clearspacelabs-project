d507b58a0586b0d6e79d6868695035b7ca3e51b0
Solomon <mcbryansolomon@yahoo.com>
2021-07-17 23:06:01 +0000

    Done with App

--- src/navigation/DrawerNavigation.js
+++ src/navigation/DrawerNavigation.js
@@ -5,7 +5,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { colors, dimensions } from "../constants";
 import Assessments from "../views/screens/Assessments/Assessments";
-import ConditionLibrary from "../views/screens/ConditionLibrary/ConditionLibrary";
 import CustomDrawer from "./CustomDrawer";
 
 const Drawer = createDrawerNavigator();
@@ -24,7 +23,6 @@ function DrawerNavigation(navigation) {
         overlayColor={colors.whiteOverlay}
       >
         <Drawer.Screen name="assessment" component={Assessments} />
-        <Drawer.Screen name="condition" component={ConditionLibrary} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
